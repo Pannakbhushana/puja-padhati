47cad1f5189c1a8695442d4baf8e4c1769c6d315
Pannakbhushana <rahulkumarmishra5338@gmail.com>
2024-08-27 10:59:15 +0530

    stuti added

--- puja-path/src/Components/StutiAccordions.jsx
+++ puja-path/src/Components/StutiAccordions.jsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { Accordion, AccordionItem, AccordionPanel, Text, Box, AccordionButton, AccordionIcon } from '@chakra-ui/react'
 
-export const StutiAccordions = ({title,description}) => {
-    return <>
+export const StutiAccordions = ({title, description}) => {
+    const formattedDescription = description.split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+            {line}
+            <br/>
+        </React.Fragment>
+    ));
+
+    return (
         <Accordion allowMultiple>
             <AccordionItem w={['100%','100%','50%']} ml={['0', '0', '25%']} borderColor={'rgba(255, 255, 255, 0.3)'}>
                 <AccordionButton>
                     <Box as='span' flex='1'>
-                        <Text fontSize='md' as={'b'} >{title}  </Text>   
+                        <Text fontSize='md' as={'b'}>{title}</Text>   
                     </Box>
                     <AccordionIcon />
                 </AccordionButton>
-                <AccordionPanel pb={4} >
-                    <Box display={"flex"} justifyContent={'center'} textAlign={'justify'} >
-                        <Text fontSize='md' as={'b'} >
-                          {description}
+                <AccordionPanel pb={4}>
+                    <Box display={"flex"} justifyContent={'center'} textAlign={'left'}>
+                        <Text fontSize='md' as={'b'}>
+                            {formattedDescription}
                         </Text>
                     </Box>
                 </AccordionPanel>
             </AccordionItem>
         </Accordion>
-    </>
-}
+    );
+};
